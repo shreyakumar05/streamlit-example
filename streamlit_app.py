9aa4791e81afa49bbf46de76e6b2d47b268b5876
Shreya Kumar <74962693+shreyakumar05@users.noreply.github.com>
2023-05-11 22:17:04 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,6 @@ import streamlit as st
 import numpy as np
 import yfinance as yf
 from tensorflow import keras
-import matplotlib.pyplot as plt
-import pandas as pd
 
 # Load the Keras model
 model_1 = keras.models.load_model("keras_model.h5")
@@ -26,7 +24,6 @@ def preprocess_data(data, timesteps):
     
     return reshaped_data
 
-
 # Streamlit app
 def main():
     # Set the page title
@@ -45,31 +42,14 @@ def main():
     if not df.empty:
         # Preprocess the stock price data
         stock_price_data = df['Close'].values
-        processed_data = preprocess_data(stock_price_data)
+        timesteps = 9  # Update this value according to your LSTM model
+        processed_data = preprocess_data(stock_price_data, timesteps)
         
         # Make predictions using the model
         predicted_price = model_1.predict(processed_data)
         
-        # Create a dataframe with actual and predicted prices
-        # Create a dataframe with actual and predicted prices
-        dates = df.index[9:]  # Exclude initial dates with no predictions
-        actual_predicted_df = pd.DataFrame({'Date': dates, 'Actual': stock_price_data[9:], 'Predicted': predicted_price.flatten()})
-        actual_predicted_df['Predicted'] = np.nan  # Set all predicted values as NaN initially
-        actual_predicted_df.iloc[-predicted_price.shape[0]:, -1] = predicted_price.flatten()  # Set the predicted values at the corresponding indices
-
-        
-        # Plot the actual and predicted prices
-        fig, ax = plt.subplots(figsize=(10, 6))
-        ax.plot(actual_predicted_df['Date'], actual_predicted_df['Actual'], label='Actual')
-        ax.plot(actual_predicted_df['Date'], actual_predicted_df['Predicted'], label='Predicted')
-        ax.set_xlabel('Date')
-        ax.set_ylabel('Price')
-        ax.set_title(f"Stock Price Prediction for {stock_ticker}")
-        ax.legend()
-        st.pyplot(fig)
-        
-        # Display the actual and predicted prices
-        st.write(actual_predicted_df)
+        # Display the predicted price
+        st.write(f"The predicted price for {stock_ticker} is ${predicted_price[-1][0]:.2f}")
     else:
         st.write("No data available for the given stock ticker.")
     

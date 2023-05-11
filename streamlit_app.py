068f36f2540ef225d9135f381b6a4f5f22e54470
Shreya Kumar <74962693+shreyakumar05@users.noreply.github.com>
2023-05-11 21:55:30 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,11 +2,11 @@ import streamlit as st
 import numpy as np
 import yfinance as yf
 from tensorflow import keras
+import matplotlib.pyplot as plt
 
 # Load the Keras model
 model_1 = keras.models.load_model("keras_model.h5")
 
-# Function to preprocess the stock data
 # Function to preprocess the stock data
 def preprocess_data(data):
     # Normalize the data
@@ -50,8 +50,22 @@ def main():
         # Make predictions using the model
         predicted_price = model_1.predict(processed_data)
         
-        # Display the predicted price
-        st.write(f"The predicted price for {stock_ticker} is ${predicted_price[0][0]:.2f}")
+        # Create a dataframe with actual and predicted prices
+        dates = df.index[9:]  # Exclude initial dates with no predictions
+        actual_predicted_df = pd.DataFrame({'Date': dates, 'Actual': stock_price_data[9:], 'Predicted': predicted_price.flatten()})
+        
+        # Plot the actual and predicted prices
+        fig, ax = plt.subplots(figsize=(10, 6))
+        ax.plot(actual_predicted_df['Date'], actual_predicted_df['Actual'], label='Actual')
+        ax.plot(actual_predicted_df['Date'], actual_predicted_df['Predicted'], label='Predicted')
+        ax.set_xlabel('Date')
+        ax.set_ylabel('Price')
+        ax.set_title(f"Stock Price Prediction for {stock_ticker}")
+        ax.legend()
+        st.pyplot(fig)
+        
+        # Display the actual and predicted prices
+        st.write(actual_predicted_df)
     else:
         st.write("No data available for the given stock ticker.")
     
